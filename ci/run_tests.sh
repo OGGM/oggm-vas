4a70b49d404e3b4e8f198ba8891803a044322e69
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2026-09-18 18:41:14 +0100

    add ci

---
+++ ci/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install oggm-vas together with the current OGGM master and run the tests.
+set -e
+set -x
+
+chown -R "$(id -u):$(id -g)" "$HOME"
+
+export MPLBACKEND=agg
+
+PIP=${PIP:-pip3}
+
+# oggm-vas tracks OGGM master, not the latest release
+$PIP install --upgrade git+https://github.com/fmaussion/salem.git
+$PIP install --upgrade git+https://github.com/OGGM/oggm.git
+$PIP install -e .
+
+pytest --verbose --durations=10 oggm_vas
