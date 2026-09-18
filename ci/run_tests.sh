52f350facfbfd42d0001af3b32dff64d258b07a1
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2026-09-18 21:00:59 +0100

    setup

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -12,6 +12,6 @@ PIP=${PIP:-pip3}
 # oggm-vas tracks OGGM master, not the latest release
 $PIP install --upgrade git+https://github.com/fmaussion/salem.git
 $PIP install --upgrade git+https://github.com/OGGM/oggm.git
-$PIP install -e .
+$PIP install -e ".[tests]"
 
 pytest --verbose --durations=10 oggm_vas
--- oggm_vas/__init__.py
+++ oggm_vas/__init__.py
@@ -6,13 +6,13 @@ _doc = ('The VAS model output, with the specific mass balance, the terminus '
         'of the geometry stored in `model_diagnostics`.')
 cfg.add_to_basenames('vas_diagnostics', 'vas_diagnostics.nc', docstr=_doc)
 
+from importlib.metadata import version, PackageNotFoundError
 try:
-    from .version import version as __version__
-    from .version import isreleased as __isreleased__
-except ImportError:
-    raise ImportError('oggm-vas is not properly installed. If you are '
-                      'running from the source directory, please instead '
-                      'create a new virtual environment (using conda or '
-                      'virtualenv) and  then install it in-place by running: '
-                      'pip install -e .')
+    __version__ = version('oggm-vas')
+except PackageNotFoundError:
+    # package is not installed
+    pass
+finally:
+    del version, PackageNotFoundError
+
 from .core import *
