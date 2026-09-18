b86c9c74913ef8b784ff3e76db6f23bbedfa0a98
Fabien Maussion <fabien.maussion@bristol.ac.uk>
2026-09-18 21:21:25 +0100

    run tests differently

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -7,11 +7,12 @@ chown -R "$(id -u):$(id -g)" "$HOME"
 
 export MPLBACKEND=agg
 
-PIP=${PIP:-pip3}
+# `python -m` throughout: the console scripts are not on PATH in all images
+PYTHON=${PYTHON:-python3}
 
 # oggm-vas tracks OGGM master, not the latest release
-$PIP install --upgrade git+https://github.com/fmaussion/salem.git
-$PIP install --upgrade git+https://github.com/OGGM/oggm.git
-$PIP install -e ".[tests]"
+$PYTHON -m pip install --upgrade git+https://github.com/fmaussion/salem.git
+$PYTHON -m pip install --upgrade git+https://github.com/OGGM/oggm.git
+$PYTHON -m pip install -e ".[tests]"
 
-pytest --verbose --durations=10 oggm_vas
+$PYTHON -m pytest --verbose --durations=10 oggm_vas
